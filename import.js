ab1159581e957c15e2f837e4f5198965201b7d82
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-03-22 12:58:25 -0600

    adding essentials

--- import.js
+++ import.js
@@ -1,32 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('./_connection');
 const dotenv = require('dotenv').config();
 
 // Import seed data
-const dbSeed = require(`./seeds/images.js`);
+const jeepsSeed = require(`./seeds/images`);
 
 // Define model
-const Jeep = require(`./models/image.js`);
+const Jeep = require(`./models/image`);
 
-/*******************************/
-/* Mongoose/MongoDB Connection */
-/*******************************/
-
-mongoose.connect(process.env.MONGODB_URL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
-
-var db = mongoose.connection;
-
-db.on('error', (error) => {
-  console.log(`Connection Error: ${error.message}`)
-});
-
-db.once('open', () => {
-  console.log('Connected to DB...');
-});
-
-Jeep.insertMany(dbSeed, (error, images) => {
+Jeep.insertMany(jeepsSeed, (error, images) => {
   console.log('Data import completed.')
   console.log(images);
   mongoose.connection.close();
