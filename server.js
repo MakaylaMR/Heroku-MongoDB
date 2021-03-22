9212ce2e61204f39e61c76eee96f9cb471144caa
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-03-22 12:36:31 -0600

    adding essentials

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const mongoose = require('./_connection.js')
 
 const dotenv = require('dotenv').config()
+const dayjs = require('dayjs')
 const express = require('express')
-const api = require('./routes/images')
 const path = require('path')
+const index = require('./routes')
 
-const model = require('./models/image')
 
 const app = express()
-
 app.set('view engine','ejs')
 
 //serve static assets
 app.use(express.static(path.join(__dirname, 'public')))
 
-// //routes
-// app.use('/',image)
+//routes
+app.use('/',index)
 
 //custom 404
 app.use((req, res) => {
@@ -28,5 +27,5 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST;
 
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`)
+  console.log(`Listening on port: ${PORT} :)`)
 })
